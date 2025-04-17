f3285359371904a7d7bf5c5d5f408b207678aac3
Rifky <rifkypraditya@gmail.com>
2025-04-17 11:06:22 +0700

    menambah fungsi urutNilaiAkhir

--- CariUrut4.java
+++ CariUrut4.java
@@ -1,3 +1,18 @@
 public class CariUrut4 {
-  
+  void urutNilaiAkhir(Penilaian4[] arrayPenilaian) {
+    // urut menggunakan insertion sort
+    for (int i = 1; i < arrayPenilaian.length; i++) {
+      Penilaian4 temp = arrayPenilaian[i];
+      int j = i;
+
+      while (j > 0 && arrayPenilaian[j - 1].nilaiAkhir < temp.nilaiAkhir) {
+        arrayPenilaian[j] = arrayPenilaian[j - 1];
+        j--;
+      }
+
+      arrayPenilaian[j] = temp;
+    }
+
+    
+  }
 }
