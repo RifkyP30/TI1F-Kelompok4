e25a178a98a25ec865f6afffe7df533966fe4859
rachmadrf <rachmad17845@gmail.com>
2025-04-17 11:13:02 +0700

    menambah fungsi cariNim

--- CariUrut4.java
+++ CariUrut4.java
@@ -17,6 +17,17 @@ public class CariUrut4 {
     for (int i = 0; i < arrayPenilaian.length; i++) {
       System.out.printf("%-30s %-30s %.1f\n", arrayPenilaian[i].mahasiswa.nama, arrayPenilaian[i].mataKuliah.namaMK, arrayPenilaian[i].nilaiAkhir);
     }
-    
+
+    void cariNimMahasiswa(Mahasiswa4[] arrayMahasiswa, String nimDicari) {
+        //mencari data NIM
+        boolean ketemu = false;
+        for (int i = 0; i < arrayMahasiswa.length; i++) {
+          if (arrayMahasiswa[i].NIM.equals(nimDicari)) {
+            ketemu = true;
+            System.out.println("Data ditemukan:");
+            System.out.println("NIM: " + arrayMahasiswa[i].NIM + " | Nama: " + arrayMahasiswa[i].nama + " | Prodi: " + arrayMahasiswa[i].prodi);
+          }
+        }
+      } 
   }
 }
